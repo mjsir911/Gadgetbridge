f80f6087b398c6b6ab97ad75eac34f44879f5084
Marco Sirabella <marco@sirabella.org>
2020-01-28 20:55:45 -0500

    Implement LengthPrefixer

--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/devices/garmin/io/LengthPrefixer.java
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/devices/garmin/io/LengthPrefixer.java
@@ -1,14 +1,40 @@
 package nodomain.freeyourgadget.gadgetbridge.service.devices.garmin.io;
 
 import java.io.OutputStream;
+import java.io.IOException;
 import java.io.BufferedOutputStream;
 
 public class LengthPrefixer extends BufferedOutputStream {
+	OutputStream out;
+	int length;
 	public LengthPrefixer(OutputStream out) {
-		super(out, 1024);
+		super(out, 0xFFFF); // magic
+		this.out = out;
 	}
+	public void write(int b) throws IOException {
+		super.write(b);
+		length++;
+	}
+	public void write(byte[] b, int off, int len) throws IOException {
+		super.write(b, off, len);
+		length += len;
+	}
+	public void flush() throws IOException {
+		// go around the buffer to write at the beginning
+		out.write(length & 0xFF);
+		out.write(length >> 8);
+		super.flush();
+	}
+
 	public static void main(String args[]) {
-		
+		try (OutputStream out = new LengthPrefixer(System.out)) {
+			int b;
+			while ((b = System.in.read()) != -1) {
+				out.write(b); // oh how I wish for .transferTo()
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
 
