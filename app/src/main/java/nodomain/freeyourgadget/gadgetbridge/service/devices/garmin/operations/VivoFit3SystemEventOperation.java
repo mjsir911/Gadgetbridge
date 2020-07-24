d9a97b683aff8002f6c09ddae23bc63aa9503fdc
Marco Sirabella <marco@sirabella.org>
2020-07-24 00:38:11 -0400

    Add SystemEventOperation

---
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/devices/garmin/operations/VivoFit3SystemEventOperation.java
@@ -0,0 +1,60 @@
+/*  Copyright (C) 2020 Marco Sirabella
+
+    This file is part of Gadgetbridge.
+
+    Gadgetbridge is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Gadgetbridge is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>. */
+package nodomain.freeyourgadget.gadgetbridge.service.devices.garmin.operations;
+
+import android.os.Build;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothGattCharacteristic;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.Externalizable;
+import java.util.UUID;
+
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
+import java.io.Externalizable;
+
+import nodomain.freeyourgadget.gadgetbridge.impl.GBDevice;
+import nodomain.freeyourgadget.gadgetbridge.service.btle.AbstractBTLEOperation;
+import nodomain.freeyourgadget.gadgetbridge.service.btle.TransactionBuilder;
+import nodomain.freeyourgadget.gadgetbridge.service.btle.actions.SetDeviceStateAction;
+import nodomain.freeyourgadget.gadgetbridge.service.devices.garmin.VivoFit3Support;
+
+public class VivoFit3SystemEventOperation extends VivoFit3Operation {
+	private byte[] data;
+	{
+		messageType = 0x13a6;
+	}
+
+	public VivoFit3SystemEventOperation(VivoFit3Support support, byte[] data) {
+		super(support);
+		this.data = data;
+	}
+
+	public void writeExternal(ObjectOutput out) throws IOException {
+		out.write(data);
+	}
+
+	public void readExternal(ObjectInput in) throws IOException {
+		/* whatever */
+	}
+}
